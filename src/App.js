0a341be34fb1c164f93b36f0f4aa717e2b2023a8
m60mor <m60mor@gmail.com>
2023-08-16 22:15:42 +0200

    Setting up jsx and css code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import './styles.css';
 
 function App() {
   return (
-  null
+  <div className='calculator-container'>
+    <div className='output'>
+      <div className='output-previous'>123,0 *</div>
+      <div className='output-current'>5325,7</div>
+    </div>
+    <div className='buttons-grid'>
+      <button>C</button>
+      <button>&#8592;</button>
+      <button>&#8730;</button>
+      <button>&divide;</button>
+
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>&times;</button>
+      
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>&minus;</button>
+
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>+</button>
+
+      <button>&#177;</button>
+      <button>0</button>
+      <button>,</button>
+      <button>=</button>
+    </div>
+  </div>
   );
 }
 
